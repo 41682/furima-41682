69edb46c29023b4a68870ad6ba60fce04847b0c2
41682 <hikaru_hirahara@outlook.com>
2024-11-20 00:26:29 +0900

    モデル修正（item）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to :category
   belongs_to :sales_status
@@ -7,6 +8,7 @@ class Item < ApplicationRecord
   belongs_to :scheduled_delivery
   has_one_attached :image
 
+  validates :image, presence: true
   validates :name, presence: true
   validates :info, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "Category can't be blank"}
@@ -15,6 +17,5 @@ class Item < ApplicationRecord
   validates :prefecture_id, numericality: { other_than: 1 , message: "Prefecture can't be blank"}
   validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "Scheduled delivery can't be blank"}
   validates :price, presence: true, numericality: { in: 300..9_999_999 }, format: { with: /\A\d+\z/ }
-  validates :image, presence: true
 
 end
