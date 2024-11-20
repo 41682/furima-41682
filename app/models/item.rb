0c803e9de6069f189707ca2dea16ece49e598adc
41682 <hikaru_hirahara@outlook.com>
2024-11-20 11:20:15 +0900

    エラーメッセージ修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,11 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :name, presence: true
   validates :info, presence: true
-  validates :category_id, numericality: { other_than: 1 , message: "Category can't be blank"}
-  validates :sales_status_id, numericality: { other_than: 1 , message: "Sales status can't be blank"}
-  validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "Shipping fee status can't be blank"}
-  validates :prefecture_id, numericality: { other_than: 1 , message: "Prefecture can't be blank"}
-  validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "Scheduled delivery can't be blank"}
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, presence: true, numericality: { in: 300..9_999_999 }, format: { with: /\A\d+\z/ }
 
 end
