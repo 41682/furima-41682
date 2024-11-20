90fb9c11f428de84b59bf7caea6ba7d18b1b55c4
41682 <hikaru_hirahara@outlook.com>
2024-11-20 17:22:11 +0900

    アクション・ルーティング設定（edit・update）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,12 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
